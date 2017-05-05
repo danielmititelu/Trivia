1ff3b7b2cf26d78227a04f3bf90dda83513d7d31
Daniel Mititelu <dumitru.mititelu@napa.fi>
2017-05-05 12:04:35 +0300

    Add HumanPlayer class

---
+++ Trivia/HumanPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trivia
+{
+    public class HumanPlayer : Player
+    {
+        public override string AskQuestion(Question question, Random random)
+        {
+            return Console.ReadLine();
+        }
+    }
+}
